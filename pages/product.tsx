8df74c5ff12aa7f117ee04e077d9abb822f65080
latudimas <riswandha.ld@gmail.com>
2022-09-22 00:42:29 +0700

    Fixing routing on /product

---
+++ pages/product.tsx
@@ -0,0 +1,18 @@
+import SidebarLayout from "@components/layouts/SidebarLayout"
+import { ReactElement } from "react"
+
+export default function Product() {
+  return (
+    <>
+      <h1 className="text-blue-500">This is product page</h1>
+    </>
+  )
+}
+
+Product.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <SidebarLayout>
+      {page}
+    </SidebarLayout>
+  )
+}
